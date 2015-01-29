8a1d8ec414aadbe1a18e93a3234fe44f14f7f17b
ZucchiniSky <scottbommarito@gmail.com>
2015-01-29 15:23:08 -0500

    increase size on insert

--- Ordered_container_array.c
+++ Ordered_container_array.c
@@ -154,6 +154,7 @@ void OC_insert(struct Ordered_container* c_ptr, const void* data_ptr)
 	OC_apply_helper(c_ptr, (OC_apply_template_fp_t)OC_take_value_from_left, NULL, APPLY_INTERNAL, result.index + 1, c_ptr->size, 1);
 	printf("apply helper done\n");
 	c_ptr->array[result.index] = (void*)data_ptr;
+	c_ptr->size++;
 	printf("inserted into array\n");
 	g_Container_items_in_use++;
 }
